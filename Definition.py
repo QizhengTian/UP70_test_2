52ee46908e4496479af52fa84bc46bfb8882b765
Eason <2870708898@qq.com>
2024-11-07 17:19:46 +0800

    Add class Teacher

--- Definition.py
+++ Definition.py
@@ -5,3 +5,11 @@ class Person:
 
     def introduce(self):
         print(f"姓名: {self.name}, 年龄: {self.age}")
+
+class Teacher(Person):
+    def __init__(self, name, age, subject):
+        super().__init__(name, age)
+        self.subject = subject
+
+    def include(self):
+        print(f"姓名: {self.name}, 年龄: {self.age}, 教授科目: {self.subject}")
